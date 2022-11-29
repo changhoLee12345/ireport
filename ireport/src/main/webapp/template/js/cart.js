721e5820b0da99d04ca94a4526d14ebdb3d3aa57
newleadon <newleadon@email.com>
2022-11-29 22:38:17 +0900

    cart

--- ireport/src/main/webapp/template/js/cart.js
+++ ireport/src/main/webapp/template/js/cart.js
@@ -17,165 +17,125 @@ Number.prototype.formatNumber = function () {
 // 1,000,000 => 1000000
 console.log('1,000,000'.replace(/,/g, ''))
 
+document.addEventListener('DOMContentLoaded', function () {
+	//선택삭제.
+	document.querySelector('.basketrowcmd a:first-child').addEventListener('click', function () {
+		basket.delCheckedItem();
+	})
+	//장바구니비우기.
+	document.querySelector('.basketrowcmd a:last-child').addEventListener('click', function () {
+		basket.delAllItem();
+	})
+	//삭제버튼.
+	document.querySelectorAll('.basketcmd a').forEach(function (item) {
+		item.addEventListener('click', function () {
+			basket.delItem();
+		})
+	})
+	//수량변경.
+	document.querySelectorAll('.updown').forEach(function (item, idx) {
+		item.querySelector('input').addEventListener('keyup', function () {
+			basket.changePNum(idx + 1)
+		})
+		item.children[1].addEventListener('click', function () {
+			basket.changePNum(idx + 1);
+		})
+		item.children[2].addEventListener('click', function () {
+			basket.changePNum(idx + 1)
+		})
+	});
+})
+
 let basket = {
 	cartCount: 0,
 	cartTotal: 0,
-
-	cartList: function () {
-		//fetch, $.ajax  <= petSHop 참조
-		//템플릿리터럴(``)을  이용하거나  cloneNode()로 노드를 복사해서 append
-		fetch("./cartSelectList")
-			.then(response => response.json())
-			.then(result => this.makeList(result))
-			.catch(err => console.log(err));
-	},
-
-	makeList: function (result) {
-
-		console.log(result);
-		let basket = document.querySelector("#basket");
-		let cartTemplate = document.querySelector("#template");
-
-		for (i = 0; i < result.length; i++) {
-
-			let rowDiv = cartTemplate.childNodes[1].cloneNode(true);
-			rowDiv.setAttribute('data-id', result[i].no);
-			rowDiv.querySelector('div.pname span').textContent = result[i].product_nm;
-			rowDiv.querySelector('div.basketprice input[name="p_price"]').value = result[i].price;
-			rowDiv.querySelector('div.basketprice').childNodes[2].textContent = (result[i].price).formatNumber();
-			rowDiv.querySelector('div.num input').value = result[i].qty;
-			rowDiv.querySelector('div.sum').textContent = (result[i].qty * result[i].price).formatNumber();
-			// rowDiv.querySelector('div.basketcmd>a').href = 'javascript:void(0)';
-			rowDiv.querySelector('div.num>div.updown>input:nth-of-type(1)').setAttribute('id', 'p_num' + result[i].no);
-			rowDiv.querySelector('div.num>div.updown>input:nth-of-type(1)').setAttribute('name', 'p_num' + result[i].no);
-			rowDiv.querySelector('div.num>div.updown>input:nth-of-type(1)').setAttribute('onkeyup', 'javascript:basket.changePNum(' + result[i].no + ')');
-			rowDiv.querySelector('div.num>div.updown>span:nth-of-type(1)').setAttribute('onclick', 'javascript:basket.changePNum(' + result[i].no + ')');
-			rowDiv.querySelector('div.num>div.updown>span:nth-of-type(2)').setAttribute('onclick', 'javascript:basket.changePNum(' + result[i].no + ')');
-			basket.appendChild(rowDiv);
-			this.reCalc();
-		}
-	},
-
-	delItem: function (e) {
-		console.log('delItem:', e);
-		let no = e.target.parentNode.parentNode.parentNode.dataset.id;
-		let url = "./cartDelete?no=" + no;
-		console.log(url);
-		fetch(url)
-			.then(result => {
-				//선택한 버튼의 상품을 삭제.. 버튼.closet(".row").remove();
-				e.target.closest('.row').remove();
-				this.reCalc();
-			})
-			.catch(err => console.log(err))
-
-	},
-
-	reCalc: function () {
-		//수량, 금액 합계 계산
-		//합계 자리에 출력
-		var cartTotal = 0;
-		var cartCount = 0;
-		document.querySelectorAll('#basket>div.data').forEach(function (item) {
-			if (item.querySelector('input[type="checkbox"]').checked) {
-				checkCnt = item.querySelector('div.num input[name="p_num' + item.dataset.id + '"]');
-				if (checkCnt) {
-
-					//console.log(checkCnt);
-
-					checkItem = item.querySelector('div.sum');
-
-					var strToNum = checkItem.textContent.replace(/,/g, '');
-					var strToCnt = checkCnt.value;
-
-					cartTotal += parseInt(strToNum);
-					cartCount += parseInt(strToCnt);
-				}
-			}
-		})
-		this.cartTotal = cartTotal;
-		this.cartCount = cartCount;
-		document.querySelector('#sum_p_num>span').textContent = (cartCount).formatNumber();
-		document.querySelector('#sum_p_price>span').textContent = (cartTotal).formatNumber();
-
-	},
-
-	changePNum: function (pos) {
-		var currentElem = event.target;
-		var val = 1; // 변경수량.
-		var currentQty = currentElem.parentNode.parentNode.childNodes[1].value;
-		var currentPrice = currentElem.parentNode.parentNode.parentNode.parentNode.childNodes[1].childNodes[1].value;
-		if (currentElem.classList[2] == 'down')
-			val = currentQty == 1 ? 0 : (val * -1);
-		// 수량변경.
-		var calQty = parseInt(currentQty) + val;
-		if (val) {
-
-			// ajax call.
-			fetch('./cartUpdate', {
-					method: 'post',
-					headers: {
-						'Content-type': 'application/json;charset=utf-8'
-					},
-					body: JSON.stringify({
-						no: pos,
-						qty: calQty
-					})
-				})
-				.then(response => response.text())
-				.then(result => console.log(result))
-				.catch(err => console.log(err));
-
-			currentElem.parentNode.parentNode.childNodes[1].value = calQty;
-			// 수량 * 금액 => 재계산.
-			var calAmount = calQty * currentPrice;
-			currentElem.parentNode.parentNode.parentNode.parentNode.childNodes[5].textContent = (calAmount).formatNumber();
-
-			this.reCalc();
-		}
-	},
-
-	changePNum1: function (val) {
-		var currentElem = event.target;
-		var currentQty = currentElem.parentNode.parentNode.childNodes[1].value;
-		var currentPrice = currentElem.parentNode.parentNode.parentNode.parentNode.childNodes[1].childNodes[1].value;
-		if (currentElem.classList[2] == 'down')
-			val = currentQty == 1 ? 0 : (val * -1);
-		// 수량변경.
-		var calQty = parseInt(currentQty) + val;
-		currentElem.parentNode.parentNode.childNodes[1].value = calQty;
-		// 수량 * 금액 => 재계산.
-		var calAmount = calQty * currentPrice;
-		currentElem.parentNode.parentNode.parentNode.parentNode.childNodes[5].textContent = (calAmount).formatNumber();
-
-		this.reCalc();
-	},
-
 	delCheckedItem: function () {
-		document.querySelectorAll('#basket div.subdiv>div.check>input[type="checkbox"]:checked').forEach(function (item) {
-			console.log(item)
-			no = item.parentElement.parentElement.parentElement.dataset.id;
-			let url = "../cartDelete?no=" + no;
-
-			fetch(url)
-				.then(result => {
-					console.log(result.text());
-					//선택한 버튼의 상품을 삭제.. 버튼.closet(".row").remove();
-					// e.target.closest('.row').remove();
-					item.parentElement.parentElement.parentElement.remove();
-				})
-				.catch(err => console.log(err))
-
+		document.querySelectorAll('input[name=buy]:checked').forEach(function (item) {
+			item.parentElement.parentElement.parentElement.remove()
 		})
 		this.reCalc();
+		this.updateUI();
 	},
-
 	delAllItem: function () {
-		document.querySelectorAll('div.subdiv>div.check>input[type="checkbox"]').forEach(function (item) {
-			item.parentElement.parentElement.parentElement.remove();
+		document.querySelectorAll('.row.data').forEach(function (item) {
+			item.remove()
+		})
+		this.cartCount = 0;
+		this.cartTotal = 0;
+		this.reCalc();
+		this.updateUI();
+	},
+	reCalc: function () {
+		this.cartCount = 0;
+		this.cartTotal = 0;
+		document.querySelectorAll('div.row.data:not(.template) .p_num').forEach(function (item) {
+			console.log(item, ',', item.getAttribute('value'))
+			var count = parseInt(item.getAttribute('value'));
+			this.cartCount += count;
+			var price = item.parentElement.parentElement.previousElementSibling.firstElementChild.getAttribute('value');
+			this.cartTotal += count * price;
+			console.log(this.cartTotal)
+		}, this)
+	},
+	updateUI: function () {
+		document.querySelector('#sum_p_num').textContent = '상품개수: ' + this.cartCount.formatNumber() + '개'
+		document.querySelector('#sum_p_price').textContent = '합계금액: ' + this.cartTotal.formatNumber() + '원'
+	},
+	changePNum: function (pos) {
+		var item = document.querySelector('input[name=p_num' + pos + ']');
+		var p_num = parseInt(item.getAttribute('value'));
+		var newval = event.target.classList.contains('up') ? p_num + 1 : event.target.classList.contains('down') ? p_num - 1 : event.target.value;
+		if (parseInt(newval) < 1 || parseInt(newval) > 99) {
+			return false;
+		}
+		item.setAttribute('value', newval);
+		item.value = newval;
+		var price = item.parentElement.parentElement.previousElementSibling.firstElementChild.getAttribute('value');
+		item.parentElement.parentElement.nextElementSibling.textContent = (newval * price).formatNumber() + "원"
+		this.reCalc();
+		this.updateUI();
+	},
+	delItem: function () {
+		event.target.parentElement.parentElement.parentElement.remove()
+	},
+	cartList: function () {
+		cartItems.forEach((item, idx) => {
+			let template = document.querySelector('.row.data.template').cloneNode(true);
+			template.style.display = 'block';
+			template.classList.remove('template')
+			template.querySelector('.pname>span').textContent = item.productNm
+			template.querySelector('.basketprice>input').value = item.price
+			template.querySelector('.basketprice').childNodes[2].textContent = item.price.formatNumber() + "원"
+			template.querySelector('.updown>input').value = item.qty
+			template.querySelector('.updown>input').setAttribute('value', item.qty)
+			template.querySelector('.updown>input').setAttribute('id', 'p_num' + (idx + 1));
+			template.querySelector('.sum').textContent = (item.price * item.qty).formatNumber() + "원"
+			console.log(template)
+			document.querySelector('#basket').append(template)
 		})
 		this.reCalc();
-	},
+		this.updateUI();
+	}
 };
+
+var cartItems = [{
+		no: 1,
+		productNm: '이것이자바다',
+		qty: 2,
+		price: 12000
+	},
+	{
+		no: 2,
+		productNm: '바로하는 C#',
+		qty: 1,
+		price: 22000
+	},
+	{
+		no: 3,
+		productNm: '파이썬하기',
+		qty: 1,
+		price: 13600
+	}
+]
 
 basket.cartList();
