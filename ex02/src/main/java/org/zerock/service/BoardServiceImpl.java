3f8508b3c4a09e25b7ebe21a6fdb9b0198b29609
hansung <hansung@email>
2023-01-26 23:03:05 +0900

    g

--- ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
+++ ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -7,39 +7,46 @@ import org.springframework.stereotype.Service;
 import org.zerock.domain.BoardVO;
 import org.zerock.mapper.BoardMapper;
 
+import lombok.AllArgsConstructor;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
-@Log4j
 @Service
+@AllArgsConstructor
+@Log4j
 public class BoardServiceImpl implements BoardService {
 
 	@Setter(onMethod_ = @Autowired)
 	private BoardMapper mapper;
-	
+
 	@Override
 	public void register(BoardVO board) {
-
+		log.info("register...." + board);
+		mapper.insertSelectKey(board);
 	}
 
 	@Override
 	public BoardVO get(Long bno) {
-		return null;
+		log.info("get.....");
+		return mapper.read(bno);
 	}
 
 	@Override
 	public boolean modify(BoardVO board) {
-		return false;
+		log.info("modify....");
+		return mapper.update(board) == 1;
 	}
 
 	@Override
 	public boolean remove(Long bno) {
-		return false;
+		log.info("remove....");
+		return mapper.delete(bno) == 1;
 	}
 
 	@Override
 	public List<BoardVO> getList() {
-		return null;
+		log.info("getList.....");
+		return mapper.getList();
 	}
 
 }
