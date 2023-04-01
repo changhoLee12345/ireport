343223d994bd36cbac3204545a1824e3c8d53d8e
'leadon' <'leadon@pc.com'>
2023-04-01 19:50:28 +0900

    r

--- ireport/src/main/java/com/yedam/myserver/HomeController.java
+++ ireport/src/main/java/com/yedam/myserver/HomeController.java
@@ -1,14 +1,8 @@
 package com.yedam.myserver;
 
-import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,21 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.yedam.myserver.excel.vo.DeptSearchVO;
-
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
