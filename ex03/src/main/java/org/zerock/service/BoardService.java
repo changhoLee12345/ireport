3714df5b95149ff01eb96968973872fa9688d186
linuxie <linuxie@mail.com>
2023-04-06 14:15:51 +0900

    c

--- ex03/src/main/java/org/zerock/service/BoardService.java
+++ ex03/src/main/java/org/zerock/service/BoardService.java
@@ -7,6 +7,7 @@ import org.zerock.domain.BoardVO;
 import org.zerock.domain.Criteria;
 
 public interface BoardService {
+	
 	public void register(BoardVO board);
 	public BoardVO get(Long bno);
 	public boolean modify(BoardVO board);
