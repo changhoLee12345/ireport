c08bca1c370ada496448dcd5e8940defb671d885
lin <lin@home.com>
2024-02-05 17:15:33 +0900

    ddd

--- ex06/src/main/webapp/WEB-INF/views/sample/admin.jsp
+++ ex06/src/main/webapp/WEB-INF/views/sample/admin.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/security/tags" prefix="sec" %>
+
 <!DOCTYPE html>
 <html>
 
@@ -11,6 +14,12 @@
 <body>
     <!-- only admin -->
     <h3>sample/admin</h3>
+    
+    <p>Principal : <sec:authentication property="principal" />
+    <p>MemberVO : <sec:authentication property="principal.member" />
+    <p>Name : <sec:authentication property="principal.member.userName" />
+    <p>Id : <sec:authentication property="principal.member.user" />
+    <p>Auth : <sec:authentication property="principal.member.authList" />
 
     <a href="/customLogout">Logout</a>
 </body>
---
+++ ex06/src/test/java/org/zerock/security/MemberMapperTests.java
@@ -0,0 +1,31 @@
+package org.zerock.security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.domain.MemberVO;
+import org.zerock.mapper.MemberMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration({ "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+@Log4j
+public class MemberMapperTests {
+
+	@Setter(onMethod_ = @Autowired)
+	private MemberMapper mapper;
+
+	@Test
+	public void testRead() {
+		log.info("start..........................................");
+		MemberVO vo = mapper.read("admin90");
+		log.info("vo: " + vo);
+
+		vo.getAuthList().forEach(authVO -> log.info("lch" + authVO));
+	}
+
+}
