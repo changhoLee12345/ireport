6c9c751ab7c47afa5096753d41d3600ab63520a7
lin <lin@home.com>
2024-03-14 12:38:30 +0900

    step not important.

--- jex00/src/test/java/org/zerock/persistence/DataSourceTests.java
+++ jex00/src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -1,31 +1,30 @@
 package org.zerock.persistence;
 
+import static org.junit.Assert.fail;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
-import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.config.RootConfig;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@ContextConfiguration(classes = { RootConfig.class })
 @Log4j
 public class DataSourceTests {
 
 	@Setter(onMethod_ = @Autowired)
 	private DataSource dataSource;
-
-	@Setter(onMethod_ = @Autowired)
-	private SqlSessionFactory sqlSessionFactory;
 
 	@Test
 	public void testConnection() {
@@ -38,20 +37,4 @@ public class DataSourceTests {
 		}
 		log.info("end  : " + System.currentTimeMillis());
 	}
-
-	public void testMybatis() {
-
-		log.info("start: " + System.currentTimeMillis());
-		try {
-			SqlSession session = sqlSessionFactory.openSession();
-			Connection conn = session.getConnection();
-			log.info(session);
-			log.info(conn);
-			log.info("end  : " + System.currentTimeMillis());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-
 }
